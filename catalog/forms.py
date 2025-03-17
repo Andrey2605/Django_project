00edcaa9e9dcca2ca241099b8b1ff9895c3774da
Andrey Maishev <fatym98@gmial.com>
2025-03-17 05:40:04 +0400

    django_fin

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,7 +3,17 @@ from django.core.exceptions import ValidationError
 
 from catalog.models import Product
 
-forbidden = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+forbidden = [
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар",
+]
 
 
 class ProductForm(forms.ModelForm):
@@ -14,40 +24,43 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
 
+        self.fields["names"].widget.attrs.update(
+            {
+                "class": "form-control",  # Добавление CSS-класса для стилизации поля
+                "placeholder": "Введите названия продукта",  # Текст подсказки внутри поля
+            }
+        )
 
-        self.fields['names'].widget.attrs.update({
-            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
-            'placeholder': 'Введите названия продукта'  # Текст подсказки внутри поля
-        })
+        self.fields["description"].widget.attrs.update(
+            {
+                "class": "form-control",  # Добавление CSS-класса для стилизации поля
+                "placeholder": "Описание продукта",  # Текст подсказки внутри поля
+            }
+        )
 
+        self.fields["category"].widget.attrs.update(
+            {
+                "class": "form-control",  # Добавление CSS-класса для стилизации поля
+            }
+        )
 
-        self.fields['description'].widget.attrs.update({
-            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
-            'placeholder': 'Описание продукта'  # Текст подсказки внутри поля
-        })
-
-
-        self.fields['category'].widget.attrs.update({
-            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
-        })
-
-        self.fields['price'].widget.attrs.update({
-            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
-            'placeholder': 'Укажите цену'  # Указание типа поля как даты
-        })
+        self.fields["price"].widget.attrs.update(
+            {
+                "class": "form-control",  # Добавление CSS-класса для стилизации поля
+                "placeholder": "Укажите цену",  # Указание типа поля как даты
+            }
+        )
 
     def clean(self):
-        names = self.cleaned_data.get('names')
-        description = self.cleaned_data.get('description')
+        names = self.cleaned_data.get("names")
+        description = self.cleaned_data.get("description")
         for word in forbidden:
             if word in names.lower() or word in description.lower():
-                raise ValidationError("Название и описание не должны содержать запрещенные слова.")
+                raise ValidationError(
+                    "Название и описание не должны содержать запрещенные слова."
+                )
 
     def clean_price(self):
-        price = self.cleaned_data.get('price')
+        price = self.cleaned_data.get("price")
         if price < 0:
             raise ValidationError("Цена продукта не может быть отрицательной.")
-
-
-
-
